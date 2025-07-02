50bf9a0a1eb3d4049e11dd92808f33f87b18089b
Echoz <chris@echoz.io>
2025-07-02 15:17:08 +0200

    feat: add substituters and experimental features (#2)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
 
+  nixConfig = {
+    extra-substituters = "https://nix-community.cachix.org";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-experimental-features = "nix-command flakes";
+  };
+
   outputs =
     { self, nixpkgs, ... }:
     let
