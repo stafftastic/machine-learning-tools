e324e03cc6d5141f60c27bacb070506d16cf542e
Chris <chris@echoz.io>
2025-07-02 14:38:34 +0200

    feat: add flake with cuda-enabled packages

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      systems = [ "x86_64-linux" ];
+      eachSystem = f: lib.genAttrs systems (system: f (argsFor system));
+      argsFor = system: {
+        inherit system;
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+            cudaCapabilities = [ "6.1" ];
+          };
+        };
+      };
+    in
+    {
+      packages = eachSystem (
+        { system, pkgs, ... }:
+        {
+          default = self.packages.${system}.all;
+          all = pkgs.symlinkJoin {
+            name = "all-packages";
+            paths = lib.attrValues self.packages.${system}.python3Packages;
+          };
+
+          python3Packages = {
+            inherit (pkgs.python3Packages)
+              scipy
+              numpy
+              pandas
+              ipykernel
+              torch
+              tensorflow
+              sentence-transformers
+              scikit-learn
+              keras
+              ;
+          };
+        }
+      );
+      formatter = eachSystem ({ pkgs, ... }: pkgs.nixfmt-tree);
+    };
+}
